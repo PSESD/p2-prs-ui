4bbcc7afb37286d9668923f2edecf4df101eca01
Matt Harris <matt@dreamsis.com>
2016-06-28 17:38:47 -0700

    Fix dataset test

--- test/models/data_set_test.rb
+++ test/models/data_set_test.rb
@@ -8,13 +8,16 @@ class DataSetTest < ActiveSupport::TestCase
     end
   end
 
-  test "should save a new data set with valid name and description" do
+  test "should save a new data set with valid name and description and then delete it" do
     data_set = DataSet.new
     data_set.name = "Test Dataset"
     data_set.description = "Test description"
     assert_nothing_raised ActiveRestClient::HTTPServerException do
       data_set.create
     end
+    assert_nothing_raised ActiveRestClient::HTTPServerException do
+      data_set.destroy
+    end
   end
-
+  
 end
