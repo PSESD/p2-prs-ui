a6a4e34a5ecd8317ded9a0d2fc737e3be0cde2d2
mzily <margarett.ly@itrellis.com>
2017-04-20 10:24:16 -0700

    delete AuthorizedEntity#services_count

--- app/models/authorized_entity.rb
+++ app/models/authorized_entity.rb
@@ -3,12 +3,7 @@ class AuthorizedEntity < PrsModel
   def services_instantiated
     Service.create_objects(services)
   end
-
-  def services_count
-    return services.size if services
-    find(id).services.size
-  end
-
+  
   def main_contact
     mainContact ? mainContact : {}
   end
